bffa9f799a297febd10de9285c77ecb83ccc4d82
Yang yu Ting <tim13467@gmail.com>
2021-10-13 14:44:58 +0800

    Initial commit

---
+++ src/main/kotlin/com/yang/Hello.kt
@@ -0,0 +1,13 @@
+package com.yang
+
+fun main() {
+//    println("Hello guys")
+//    Human().hello()
+    val g = Human()
+    g.hello()
+}
+class Human {
+    fun hello (){
+        println("hello kotlin")
+    }
+}
